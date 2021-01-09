0649018382661144e7d069604d2ca4e34dc86fef
chowii <sabbib007madness@gmail.com>
2021-01-10 10:03:52 +1100

    create review model

--- campground/data/campgroundModel.js
+++ campground/data/campgroundModel.js
@@ -3,9 +3,14 @@ const Schema = mongoose.Schema
 
 const campgroundSchema = new Schema({
     title: String,
+    image: String,
     price: String,
     description: String,
-    location: String
+    location: String,
+    review: {
+        type: Schema.Types.ObjectId,
+        ref: 'Review4'
+    }
 })
 
 module.exports = mongoose.model('Campground', campgroundSchema)
