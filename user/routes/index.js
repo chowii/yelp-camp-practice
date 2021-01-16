cd03c11bc2fcd36d87c2980a9a056356138322b3
chowii <sabbib007madness@gmail.com>
2021-01-16 16:12:03 +1100

    implement register user route

--- user/routes/index.js
+++ user/routes/index.js
@@ -1,3 +1,13 @@
 const router = require('express').Router()
-// TODO implement register new user route
+const catchAsync = require('../../error/catchAsync')
+const UserModel = require('../data/userModel')
+
+router.post('/register', catchAsync(async (req, res) => {
+    const {email, username, password} = req.body
+    const user = new UserModel({email, username})
+    const registeredUser = await UserModel.register(user, password)
+    console.log(registeredUser)
+    res.send(registeredUser)
+}))
+
 module.exports = router
