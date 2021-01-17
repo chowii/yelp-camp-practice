5a56317c5ab4d0ea272acd9b01cf62eaba4a883d
chowii <sabbib007madness@gmail.com>
2021-01-17 15:19:43 +1100

    implement isAuthor middelware

---
+++ user/isAuthor.js
@@ -0,0 +1,11 @@
+const CampgroundModel = require('../campground/data/campgroundModel')
+const ApiError = require("../error/ApiError");
+
+module.exports.isAuthor = async (req, res, next) => {
+    const campground = await CampgroundModel.findById(req.params.id).populate('author')
+    if (req.user.id === campground.author.id) {
+        next()
+    } else {
+        next(new ApiError("unauthorized", 401))
+    }
+}
